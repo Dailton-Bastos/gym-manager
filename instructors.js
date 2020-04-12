da902e0dbb8f61d7440b80d6d49bbd063d7d8d2b
Dailton Bastos <dailtonbastos@gmail.com>
2020-04-12 19:26:58 -0300

    Atualizando e excluindo instrutores

--- instructors.js
+++ instructors.js
@@ -1,5 +1,27 @@
 const fs = require('fs');
 const data = require('./data.json');
+const { getAge, getBirth } = require('./utils');
+
+exports.show = (req, res) => {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find((instructor) => {
+    return instructor.id === +id;
+  });
+
+  if (!foundInstructor) return res.send('Instructor not found!');
+
+  const instructor = {
+    ...foundInstructor,
+    age: getAge(foundInstructor.birth),
+    services: foundInstructor.services.split(', '),
+    created_at: new Intl.DateTimeFormat('pt-BR').format(
+      foundInstructor.created_at
+    ),
+  };
+
+  return res.render('instructors/show', { instructor });
+};
 
 exports.post = (req, res) => {
   const keys = Object.keys(req.body);
@@ -24,9 +46,70 @@ exports.post = (req, res) => {
     created_at: Date.now(),
   });
 
-  // eslint-disable-next-line consistent-return
   fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send('Write file error');
+    return true;
   });
   return res.redirect('/instructors');
+};
+
+exports.edit = (req, res) => {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find((instructor) => {
+    return instructor.id === +id;
+  });
+
+  const instructor = {
+    ...foundInstructor,
+    birth: getBirth(foundInstructor.birth),
+  };
+
+  if (!foundInstructor) return res.send('Instructor not found!');
+
+  return res.render('instructors/edit', { instructor });
+};
+
+exports.put = (req, res) => {
+  const { id } = req.body;
+
+  let index = 0;
+  const foundInstructor = data.instructors.find((instructor, foundIndex) => {
+    index = foundIndex;
+    return instructor.id === +id;
+  });
+
+  if (!foundInstructor) return res.send('Instructor not found!');
+
+  const instructor = {
+    ...foundInstructor,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+    id: +id,
+  };
+
+  data.instructors[index] = instructor;
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send('Write file error');
+    return true;
+  });
+  return res.status(200).redirect(`/instructors/${id}`);
+};
+
+exports.delete = (req, res) => {
+  const { id } = req.body;
+
+  const filteredInstructors = data.instructors.filter((instructor) => {
+    return instructor.id !== +id;
+  });
+
+  data.instructors = filteredInstructors;
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send('Write file error');
+    return true;
+  });
+
+  return res.status(200).redirect('/instructors/');
 };
--- routes.js
+++ routes.js
@@ -17,6 +17,14 @@ routes.get('/instructors/create', (req, res) => {
 
 routes.post('/instructors', instructors.post);
 
+routes.get('/instructors/:id', instructors.show);
+
+routes.get('/instructors/:id/edit', instructors.edit);
+
+routes.put('/instructors', instructors.put);
+
+routes.delete('/instructors', instructors.delete);
+
 routes.get('/members', (req, res) => {
   return res.render('members/index');
 });
