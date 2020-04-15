35ba79c8651976f4034a31c4aa48699d2feb6de5
Dailton Bastos <dailtonbastos@gmail.com>
2020-04-14 22:16:28 -0300

    Listando instrutores

--- instructors.js
+++ instructors.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const data = require('./data.json');
 const { getAge, getBirth } = require('./utils');
 
+const { instructors } = data;
+
+exports.index = (req, res) => {
+  return res.render('instructors/index', { instructors });
+};
+
 exports.show = (req, res) => {
   const { id } = req.params;
 
